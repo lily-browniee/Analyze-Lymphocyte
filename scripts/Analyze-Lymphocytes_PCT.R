f8966b7ea4894c480948084dcac8f9014587547c
lily-browniee <56736987+lily-browniee@users.noreply.github.com>
2020-05-05 16:11:37 -0400

    Update Analyze-Lymphocytes_PCT.R

--- scripts/Analyze-Lymphocytes_PCT.R
+++ scripts/Analyze-Lymphocytes_PCT.R
@@ -1,5 +1,6 @@
 
-## BTEC330 Project2 Velecina
+## BTEC330 Project2
+## By Hana Velecina
 
 ## Install necessary packages
 install.packages("ggplot2")
@@ -76,6 +77,47 @@ Lymphocytes_PCT_boxplot <- boxplot(Lymphocytes_PCT ~ IBS.subtype, data = IBS, ma
 )
 print(Lymphocytes_PCT_boxplot)
 dev.off()
+
+## BTEC395 spring2020 Final Project new lines of codes
+
+setwd("./")
+
+## Install necessary packages
+install.packages("ggplot2")
+library(ggplot2)
+
+## Read data
+IBS <- read.csv("../data/GSE124549_20200330.csv", header = TRUE)
+
+head(IBS)
+
+
+## Recursive analysis for regression  - RNA Expression
+## https://stackoverflow.com/questions/42464767/how-to-run-lm-regression-for-every-column-in-r
+## https://stackoverflow.com/questions/44170937/performing-lm-and-segmented-on-multiple-columns-in-r
+## http://www.learnbymarketing.com/tutorials/explaining-the-lm-summary-in-r/
+## https://tutorials.iq.harvard.edu/R/Rstatistics/Rstatistics.html
+
+
+## Access only the columns with RNA Expression
+names(IBS)[37:286]
+
+##  Make a data frame of list type
+storage <- list()
+
+## linear regression for each expressed gene
+for(i in names(IBS)[37:286]){
+  storage[[i]]  <- lm(get(i) ~ Lymphocytes_PCT...., IBS)
+}
+
+summary(storage$AGO2)
+summary(storage$AGO2)$r.squared
+summary(storage$AGO2)$coefficients[,4]
+
+## output the results of the 250 genes in data_output folder
+sink('../data_output/Lymphocytes_PCT_storage.txt', append = TRUE)
+print(storage)
+sink()
 
 
 
